feb3ef87231af840a72afca7d936f9f1b7a2baac
Mark Phillips <markp@hey.com>
2025-06-18 11:43:54 +0100

    Updates to nvim configs

--- config/nvim/after/plugin/mason.lua
+++ config/nvim/after/plugin/mason.lua
@@ -1,1 +1,13 @@
 require("mason").setup()
+
+require("mason-tool-installer").setup({
+    ensure_installed = {
+        "terraform-ls",
+        "ansible-language-server",
+        "tailwindcss-language-server",
+        "terraform",
+        "tflint",
+        "ansible-lint",
+        "jsonlint",
+    },
+})
--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,3 +1,7 @@
 return {
-	"williamboman/mason.nvim",
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim", -- optional but recommended
+    },
 }
