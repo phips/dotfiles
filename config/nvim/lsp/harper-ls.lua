8005d823a75d795e5eadb36b17c44facf8c240ac
Mark Phillips <markp@hey.com>
2025-07-21 17:11:26 +0100

    add harper as an OS package, config here

--- config/nvim/lsp/harper-ls.lua
+++ config/nvim/lsp/harper-ls.lua
@@ -1,3 +1,32 @@
 return {
     cmd = { "harper-ls", "--stdio" },
+    settings = {
+        ["harper-ls"] = {
+            userDictPath = "",
+            fileDictPath = "",
+            linters = {
+                SpellCheck = true,
+                SpelledNumbers = false,
+                AnA = true,
+                SentenceCapitalization = true,
+                UnclosedQuotes = true,
+                WrongQuotes = false,
+                LongSentences = true,
+                RepeatedWords = true,
+                Spaces = true,
+                Matcher = true,
+                CorrectNumberSuffix = true,
+            },
+            codeActions = {
+                ForceStable = false,
+            },
+            markdown = {
+                IgnoreLinkTitle = false,
+            },
+            diagnosticSeverity = "hint",
+            isolateEnglish = false,
+            dialect = "British",
+            maxFileLength = 120000,
+        },
+    },
 }
