47f05f78ad051f2765a90764a15f173d95719f55
Mark Phillips <mark@freebsdfoundation.org>
2025-11-08 16:56:57 +0000

    nvim: tailwind LSP config

--- config/nvim/lsp/tailwindcss-language-server.lua
+++ config/nvim/lsp/tailwindcss-language-server.lua
@@ -2,7 +2,7 @@
 return {
     cmd = { "tailwindcss-language-server", "--stdio" },
     filetypes = { "gohtml", "gohtmltmpl", "html", "css", "postcss", "sass", "scss" },
-    root_markers = { "hugo.yaml" },
+    root_markers = { "hugo.yaml", "hugo.toml", "theme.toml" },
     -- settings = {
     --     "tailwindCSS.experimental.configFile" = "assets/css/main.css"
     -- },
